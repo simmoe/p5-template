b4d7b46b33d12550cd449eb527b2c83aefc75229
Simon <simmoe@gmail.com>
2022-02-10 08:16:40 +0100

    first commit

---
+++ sketch.js
@@ -0,0 +1,110 @@
+let capture;
+
+function setup() {
+  createCanvas(390, 240);
+  capture = createCapture(VIDEO);
+  capture.size(320, 240);
+  //capture.hide();
+}
+
+function draw() {
+  background(255);
+  image(capture, 0, 0, 320, 240);
+  filter(INVERT);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const mqttInit = () => {
+  //opret et id med en random talkode og sæt gem servernavnet i en variabel
+  const clientId = 'mqttjs_' + Math.random().toString(16).substr(2, 8)
+  const host = 'wss://mqtt.nextservices.dk'
+
+  //opret et objekt med de oplysninger der skal bruges til at forbinde til serveren
+  const options = {
+    keepalive: 300,
+    clientId: clientId,
+    protocolId: 'MQTT',
+    protocolVersion: 4,
+    clean: true,
+    reconnectPeriod: 1000,
+    connectTimeout: 30 * 1000,
+    will: {
+      topic: 'WillMsg',
+      payload: 'Connection Closed abnormally..!',
+      qos: 0,
+      retain: false
+    },
+    rejectUnauthorized: false
+  }
+
+  console.log('connecting mqtt client')
+
+  //forsøg at oprette forbindelse 
+  client = mqtt.connect(host, options)
+
+  //hvis der sker en fejl kaldes denne funktion
+  client.on('error', (err) => {
+    console.log('Connection error: ', err)
+    client.end()
+  })
+
+  //og hvis forbindelsen mistes kaldes denne funktion
+  client.on('reconnect', () => {
+    console.log('Reconnecting...')
+  })
+
+  //hvis forbindelsen lykkes kaldes denne funktion
+  client.on('connect', () => {
+    console.log('Client connected:' + clientId)
+  })
+
+  //når forbindelsen lukkes kaldes denne funktion
+  client.on('close', () => {
+    console.log(clientId + ' disconnected')
+  })
+} 
+
+function showChart(){
+  //opret chart 
+  chart = new Chart(select('#chartCanvas').elt, {
+      type: 'bar',
+      data: {
+        labels: ['hej', 'med', 'dig'],
+        datasets: [{
+            label: 'Resultat',
+            data: [33,10,23],
+              backgroundColor: ['lightred', 'lightgreen', 'lightblue'],
+              borderWidth: 3
+          }]
+      },
+  });
+}
